96dd333349b86be1296b03eb47b1fa09e3d708a1
Kelvin Machira <machirakelvin81@gmail.com>
2024-05-11 00:04:31 +0300

    routes

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,16 +1,47 @@
-import { useEffect, useState } from "react";
 
-function Actors() {
+import React from 'react';
+import NavBar from "../components/NavBar";
+const actors = [
+  {
+    name: "Benedict Cumberbatch",
+    movies: ["Doctor Strange", "The Imitation Game", "Black Mass"],
+  },
+  {
+    name: "Justin Timberlake",
+    movies: ["Trolls", "Friends with Benefits", "The Social Network"],
+  },
+  {
+    name: "Anna Kendrick",
+    movies: ["Pitch Perfect", "Into The Woods"],
+  },
+  {
+    name: "Tom Cruise",
+    movies: [
+      "Jack Reacher: Never Go Back",
+      "Mission Impossible 4",
+      "War of the Worlds",
+    ],
+  },
+];
+
+const ActorsPage = () => {
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Actor info here! */}
-      </main>
-    </>
+    <div>
+      <h1>Actors Page</h1>
+      <ul>
+        {actors.map((actor, index) => (
+          <li key={index}>
+            <strong>{actor.name}</strong>:
+            <ul>
+              {actor.movies.map((movie, index) => (
+                <li key={index}>{movie}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
-export default Actors;
+export default ActorsPage;
